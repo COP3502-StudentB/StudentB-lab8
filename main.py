02303a80f7fbe6cb43a2a71d96e8f6df261528df
jereme-yang <jeremeyang@ufl.edu>
2024-04-10 22:50:37 -0400

    Student B's first commit!
    does anyone read these

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+
+STUDENT B's main.py file
+
+'''
+
+def encode(password):
+    '''encodes the password by modifying each digit
+
+    :param password: string of 8 integers
+    :return: encoded password
+    '''
+    pass
+
+if __name__ == "__main__":
+    '''
+    while loop for menu goes here
+    '''
+    while True:
+        break
